1d31f6a78341b1cce60ef98ae05ebb1b2a4ecc42
alex garag <alex.garag@bk.ru>
2024-08-10 12:08:25 +0300

    Задача_3 сделана

--- src/by/tms/lesson20/onl30/thread/ExecutorTrainingManual.java
+++ src/by/tms/lesson20/onl30/thread/ExecutorTrainingManual.java
@@ -1,6 +1,7 @@
 package by.tms.lesson20.onl30.thread;
 
 import by.tms.lesson20.onl30.thread.trainingmanual.Child;
+import by.tms.lesson20.onl30.thread.trainingmanual.MyRunnable;
 
 public class ExecutorTrainingManual {
     public static void main(String[] args) {
@@ -9,7 +10,7 @@ public class ExecutorTrainingManual {
 //        main первой строкой вывести в консоль “Поток main начинает свою работу” затем создать
 //        объект Child и запустить поток. Затем на последней строке метода main вывести в
 //        консоль “Поток main завершает свою работу”. Посмотреть результат и сделать вывод
-//        System.out.println("Задача 1");
+//        System.out.println("\"Задача 1\"");
 //        System.out.println("Поток main начинает свою работу");
 //        Child child = new Child("child");
 //        child.start();
@@ -18,23 +19,30 @@ public class ExecutorTrainingManual {
 //        Задача 2:
 //        С помощью метода join() изменить предыдущий код таким образом, чтобы надпись “Поток
 //        main завершает свою работу” всегда была последней
-        System.out.println("\nЗадача 2");
-        System.out.println("Поток main начинает свою работу");
-
-        Child child1 = new Child("child1");
-        child1.start();
-
-        try {
-            child1.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+//        System.out.println("\n\"Задача 2\"");
+//        System.out.println("Поток main начинает свою работу");
+//
+//        Child child1 = new Child("child1");
+//        child1.start();
+//
 //        try {
-//            Thread.sleep(5000);
+//            child1.join();
 //        } catch (InterruptedException e) {
 //            throw new RuntimeException(e);
 //        }
+////        try {
+////            Thread.sleep(5000);
+////        } catch (InterruptedException e) {
+////            throw new RuntimeException(e);
+////        }
+//        System.out.println("Поток main завершает свою работу");
+//        Задача 3:
+//        Переписать предыдущий код используя интерфейс Runnable
+        System.out.println("\n\"Задача 3\"");
+        System.out.println("Поток main начинает свою работу");
+//        MyRunnable myRunnable = new MyRunnable();
+        MyRunnable myRunnable = new MyRunnable("myRunnable");
+        myRunnable.run();
         System.out.println("Поток main завершает свою работу");
-
     }
 }
---
+++ src/by/tms/lesson20/onl30/thread/trainingmanual/MyRunnable.java
@@ -0,0 +1,13 @@
+package by.tms.lesson20.onl30.thread.trainingmanual;
+
+public class MyRunnable extends Thread implements Runnable {
+    public MyRunnable(String name) {
+        super(name);
+    }
+    @Override
+    public void run() {
+        for (int i = 1; i < 101; i++) {
+            System.out.println(i);
+        }
+    }
+}
