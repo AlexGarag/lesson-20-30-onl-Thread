ef0ba56ae6174bbb0b14fdd147db8fc5c82d2f48
alex garag <alex.garag@bk.ru>
2024-08-12 04:39:48 +0300

    Начал решать Задачу*

---
+++ src/by/tms/lesson20/onl30/thread/LauncherAsterisk.java
@@ -0,0 +1,13 @@
+package by.tms.lesson20.onl30.thread;
+
+public class LauncherAsterisk {
+    public static final String MESSAGE_MANUFACTURER = "The Manufacturer delivered the product to the Store";
+
+    public static void main(String[] args) {
+
+//        Имеются сущности Магазин, Производитель, Покупатель. Цель задачи сделать так чтобы
+//        производитель произвел 5 единиц продукта, а покупатель их купил. Пока производитель
+//        не произвел продукт, покупатель не может его купить. При этом одновременно в магазине
+//        может находиться не более 3 товаров
+    }
+}
---
+++ src/by/tms/lesson20/onl30/thread/asterisk/Manufacturer.java
@@ -0,0 +1,10 @@
+package by.tms.lesson20.onl30.thread.asterisk;
+
+public class Manufacturer implements Runnable {
+
+
+    @Override
+    public void run() {
+        System.out.println("Manufacturer");
+    }
+}
